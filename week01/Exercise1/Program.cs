872d7b930ccabc5e2b7b2b931087a56aabb4672d
pranawijaya <pranajobs@hotmail.com>
2025-03-03 22:30:39 +0700

    week01-csharp-1

--- week01/Exercise1/Program.cs
+++ week01/Exercise1/Program.cs
@@ -2,8 +2,14 @@ using System;
 
 class Program
 {
-    static void Main(string[] args)
+    static void Main()
     {
-        Console.WriteLine("Hello World! This is the Exercise1 Project.");
+        Console.Write("What is your first name? ");
+        String firstName = Console.ReadLine();
+
+        Console.Write("What is your last name? ");
+        String lastName = Console.ReadLine();
+
+        Console.WriteLine($"\nYour name is {lastName}, {firstName} {lastName}.");
     }
 }
